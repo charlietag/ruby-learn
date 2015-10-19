2001ea51db182f78493628fbc25ba57fc753c6fd
Charlie Hsieh <charlieofficial@gmail.com>
2015-10-19 08:54:55 +0800

    split usage into array

--- split.rb
+++ split.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 
-letters = ['a', 'b', 'c', 'd']
-letters.each do |letter|
-  puts letter
+puts "first type:"
+text = gets.chomp
+words = text.split(",")
+words.each do |word|
+  puts word
 end

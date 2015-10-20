62885a00fe5565374d3fb7b3e0b21d5b087435c6
Charlie Hsieh <charlieofficial@gmail.com>
2015-10-20 08:57:15 +0800

    try hash concept

---
+++ hash.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+pets = Hash.new
+pets["Name"] = "Charlie"
+puts pets["Name"]
+
+# Above equals to 
+animals = {} # equals to animals = Hash.new
+animals["Name"] = "Haha"
+puts animals["Name"]

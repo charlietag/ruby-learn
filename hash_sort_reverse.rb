1d90f3968b81abe35a18d7fd20b9d8593a3072fa
Charlie Hsieh <charlieofficial@gmail.com>
2015-10-27 08:21:14 +0800

    sort and reverse string

---
+++ hash_sort_reverse.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+frequences = Hash.new(0)
+words = ["a","b","c","c","b","b","b"]
+words.each do |word|
+      frequences[word] +=1
+end
+
+puts frequences
+frequences.each do |x,y|
+  puts "#{x}:#{y}"
+end
+frequences = frequences.sort_by do |word,count|
+  count
+end
+puts ""
+
+frequences.reverse!
+
+puts frequences
+frequences.each do |x,y|
+  puts "#{x}:#{y}"
+end
+
+puts ""
+frequences.reverse!
+
+frequences.each do |x,y|
+  puts "#{x}:#{y}"
+end

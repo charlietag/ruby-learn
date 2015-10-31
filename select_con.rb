df05fb8f2a6b7d7e44f9a6a89c61d650c3275ebe
Charlie Hsieh <charlieofficial@gmail.com>
2015-11-01 01:39:13 +0800

    1101

---
+++ select_con.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+movie_ratings = {
+  memento: 3,
+  primer: 3.5,
+  the_matrix: 5,
+  truman_show: 4,
+  red_dawn: 1.5,
+  skyfall: 4,
+  alex_cross: 2,
+  uhf: 1,
+  lion_king: 3.5
+}
+# Add your code below!
+
+good_movies = movie_ratings.select do |x,y|
+    y > 3
+end

dbcd2398eb6072cc175ab1d72e71f0803bdd7f90
Charlie Hsieh <charlieofficial@gmail.com>
2015-11-01 22:02:56 +0800

    1101

--- script.rb
+++ script.rb
@@ -1,30 +1,41 @@
 #!/usr/bin/env ruby
 
+movies = {
+    movie: "ano",
+    rating: 3
+}
+puts "Enter your choice:"
+choice = gets.chomp
 
-puts '==========='
-def sample_method ( arg1 = :arg1, argN = :argN )
-	puts arg1
-	puts argN
+case choice
+when "add"
+  puts "Enter a movie title:"
+  title = gets.chomp
+  if movies[title.to_sym].nil?
+      puts "Enter the rating:"
+      rating = gets.chomp
+      movies[title.to_sym] = rating.to_i
+  else
+      puts "#{title} already exists"
+  end
+
+  puts "#{title} is in rating #{movies[title]}"
+when "update"
+  puts "Which one you would update:"
+  title = gets.chomp
+  if movies[title.to_sym].nil?
+      puts "err:not found"
+  else
+      puts "rating you would like to put:"
+      rating = gets.chomp
+      movies[title.to_sym] = rating.to_i
+  end
+when "display"
+  movies.each do |x,y|
+      puts "#{x}: #{y}"
+  end
+when "delete"
+  puts "deleted!"
+else
+  puts "Error!"
 end
-sample_method()
-puts '--------------------'
-sample_method('giving arg1', 'giving argN')
-puts '==========='
-state_abbrev = {}
-state_abbrev[:ALABAMA] = 'AL'
-state_abbrev[:CALIFORNIA] = 'CA'
-state_abbrev[:DELAWARE] = 'DE'
-state_abbrev[:FLORIDA] = 'FL'
-state_abbrev[:KANSAS] = 'KS'
-puts state_abbrev[:DELAWARE] 
-puts '--------------------'
-puts state_abbrev.index('CA')
-puts '==========='
-weekend = {:sat => 'Saturday', :sun => 'Sunday'}
-puts weekend[:sat]
-puts '--------------------'
-puts weekend.keys
-puts '--------------------'
-puts weekend.values
-puts '==========='
-

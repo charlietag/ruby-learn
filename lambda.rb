465927bde6da0d2b5e070b701ef47e4c6230e2f3
Charlie Hsieh <charlieofficial@gmail.com>
2015-11-09 08:28:46 +0800

    1109 lambda

--- lambda.rb
+++ lambda.rb
@@ -1,4 +1,9 @@
 #!/usr/bin/env ruby
 
-lambda { puts "echo test"}
-lambda.call
+Proc.new { puts "echo test Proc" }.call
+my_proc = Proc.new { puts "echo test Proc" }
+my_proc.call
+
+lambda { puts "echo test lambda"}.call
+my_lambda = lambda { puts "echo test lambda"}
+my_lambda.call

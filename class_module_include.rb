6c86405bd8649f476274ce7d91a46fe89c27e00d
Charlie Hsieh <charlieofficial@gmail.com>
2015-11-17 22:15:06 +0800

    1117 module

---
+++ class_module_include.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+#put test script here
+class Angle
+  include Math
+  attr_accessor :radians
+
+  def initialize(radians)
+    @radians = radians
+  end
+
+  def cosine
+    cos(@radians)
+  end
+end
+
+acute = Angle.new(1)
+puts acute.cosine
---
+++ class_module_require.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+
+#put test script here
+require 'date'
+
+puts Date.today

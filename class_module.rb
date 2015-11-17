c6ab69911d35220cf1e4301945e49ad751d98abb
Charlie Hsieh <charlieofficial@gmail.com>
2015-11-17 08:27:00 +0800

    1117

---
+++ class_module.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+module Circle
+
+  PI = 3.141592653589793
+  
+  def Circle.area(radius)
+    PI * radius**2
+  end
+  
+  def Circle.circumference(radius)
+    2 * PI * radius
+  end
+end

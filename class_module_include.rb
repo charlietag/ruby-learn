f68f31a8738fc4578faabec90bc520385026809e
Charlie Hsieh <charlieofficial@gmail.com>
2015-11-18 08:57:42 +0800

    1118 module include (mixin) vs extend

--- class_module_include.rb
+++ class_module_include.rb
@@ -1,18 +1,32 @@
 #!/usr/bin/env ruby
 
 #put test script here
-class Angle
-  include Math
-  attr_accessor :radians
-
-  def initialize(radians)
-    @radians = radians
-  end
-
-  def cosine
-    cos(@radians)
+module Action
+  def jump
+    @distance = rand(4) + 2
+    puts "I jumped forward #{@distance} feet!"
   end
 end
 
-acute = Angle.new(1)
-puts acute.cosine
+class Rabbit
+  extend Action
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+end
+
+class Cricket
+  include Action
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+end
+
+peter = Rabbit.new("Peter")
+#jiminy = Cricket.new("Jiminy")
+puts Rabbit.jump
+
+#peter.jump
+#jiminy.jump

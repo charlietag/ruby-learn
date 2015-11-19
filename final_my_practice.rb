0922fd7b92c5d55ebed6edc16e869a565456805b
Charlie Hsieh <charlieofficial@gmail.com>
2015-11-19 22:06:58 +0800

    1119

---
+++ final_my_practice.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+#put test script here
+class Account
+  attr_reader :name
+  attr_reader :balance
+  def initialize(name,balance=100)
+    @name = name
+    @balance = balance
+  end
+
+  def display_balance(pin_number)
+    puts pin_number == self.pin ? "Balance: $#{@balance}." : self.pin_error
+  end
+
+  def withdraw(pin_number,amount)
+    if pin_number = self.pin
+      @balance -= amount
+      puts "Withdrew #{amount}, New balance: $#{@balance}."
+    else
+      puts self.pin_error
+    end
+  end
+
+
+  private
+  def pin
+    @pin = 1234
+  end
+  def pin_error
+    "Access denied: incorrect PIN."
+  end
+end

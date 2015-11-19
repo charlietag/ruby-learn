b48fc2ff6c3296161531a4c605e204a1530d5519
Charlie Hsieh <charlieofficial@gmail.com>
2015-11-19 22:12:04 +0800

    add if notice

---
+++ if_notice.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+#put test script here
+a = 1
+b = 2
+if a == b
+  puts "this means action , compare if a == b"
+end
+
+if a = b
+  puts "this means return true or false boolen, after assign a = b"
+end

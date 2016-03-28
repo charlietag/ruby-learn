3d6b5f4e50d4440265ea68349d310663ab4fcecb
Charlie Hsieh <charlieofficial@gmail.com>
2016-03-29 00:42:43 +0800

    change coding style

--- histogram.rb
+++ histogram.rb
@@ -1,11 +1,28 @@
 #!/usr/bin/env ruby
 
-puts "Text please: "
+puts "Text please: (Give at lease two the same string)"
 text = gets.chomp
 
+# Put my words into array
 words = text.split(" ")
+
+# Default value for each hash in frequencies
 frequencies = Hash.new(0)
-words.each { |word| frequencies[word] += 1 }
-frequencies = frequencies.sort_by {|a, b| b }
+
+# Iterate words, and use symbol in hash
+words.each do |word| 
+  frequencies[word.to_sym] += 1 
+end
+
+# After count above, now sort by count number asc
+frequencies = frequencies.sort_by do |a, b|
+  b
+end
+
+# Let's change the sort to desc
 frequencies.reverse!
-frequencies.each { |word, frequency| puts word + " " + frequency.to_s }
+
+# Now display the result
+frequencies.each do |word, frequency|
+  puts "#{word} #{frequency}"
+end
